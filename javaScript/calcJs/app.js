eb835268b44a04a211243301cc51566a9dbb7957
frontendprof <https://github.com/frontendprof>
2020-12-14 12:12:34 +0500

    Coma-separated value logic

--- javaScript/calcJs/app.js
+++ javaScript/calcJs/app.js
@@ -1,10 +1,53 @@
 
-// function getHistory(){
-//     return document.getElementById("his-val").innerText;
-// }
-// // alert(getHistory())
+function getHistory(){
+    return document.getElementById("hist-val").innerText;
+}
+// alert(getHistory())
 
-// function printHistory(num){
-//     document.getElementById("his-val").innerText=num;
-// }
+function printHistory(num){
+    document.getElementById("hist-val").innerText=num;
+}
 // printHistory("8*2")
+
+function getOutput(){
+    return document.getElementById("output-val").innerText;
+}
+
+
+function printOutput(num){
+    document.getElementById("output-val").innerText=getFormattedNumber(num);
+}
+
+// printOutput(123435345345)
+
+// Getting coma separated numbers
+function getFormattedNumber(num){
+    let n=Number(num);
+    let val=n.toLocaleString("en");
+    return val;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// toLocaleString() ====> 212312,,41234,1234
