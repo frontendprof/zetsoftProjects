316dec5197bb7940e3df84b1f7139fcd746c95f5
frontendprof <https://github.com/frontendprof>
2020-12-14 12:00:44 +0500

    Styling keyboard and output section

---
+++ javaScript/calcJs/app.js
@@ -0,0 +1,10 @@
+
+// function getHistory(){
+//     return document.getElementById("his-val").innerText;
+// }
+// // alert(getHistory())
+
+// function printHistory(num){
+//     document.getElementById("his-val").innerText=num;
+// }
+// printHistory("8*2")
